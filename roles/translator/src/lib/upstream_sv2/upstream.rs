89947f76eb442ae0479fc436455c3a658bb6a6e9
vnprc <vnprc@protonmail.com>
2024-12-03 12:25:58 -0500

    get it running again
    change Sv2KeySet to only have one signing key
    encoding a variable sized keyset was too difficult
    tackle that problem later, maybe
    
    encode Sv2SigningKey using PubKey
    lifetimes...lifetimes everywhere
    TODO: maybe change back to a struct without a lifetime

--- roles/translator/src/lib/upstream_sv2/upstream.rs
+++ roles/translator/src/lib/upstream_sv2/upstream.rs
@@ -102,7 +102,7 @@ pub struct Upstream {
     // than the configured percentage
     pub(super) difficulty_config: Arc<Mutex<UpstreamDifficultyConfig>>,
     task_collector: Arc<Mutex<Vec<(AbortHandle, String)>>>,
-    keyset: Arc<Mutex<Option<Sv2KeySet>>>,
+    keyset: Arc<Mutex<Option<Sv2KeySet<'static>>>>,
     wallet: Arc<Mutex<Wallet>>,
     premint_secrets: Arc<Mutex<Option<PreMintSecrets>>>,
 }
@@ -132,7 +132,7 @@ impl Upstream {
         target: Arc<Mutex<Vec<u8>>>,
         difficulty_config: Arc<Mutex<UpstreamDifficultyConfig>>,
         task_collector: Arc<Mutex<Vec<(AbortHandle, String)>>>,
-        keyset: Arc<Mutex<Option<Sv2KeySet>>>,
+        keyset: Arc<Mutex<Option<Sv2KeySet<'static>>>>,
         wallet: Arc<Mutex<Wallet>>,
         premint_secrets: Arc<Mutex<Option<PreMintSecrets>>>,
     ) -> ProxyResult<'static, Arc<Mutex<Self>>> {
